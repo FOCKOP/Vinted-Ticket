8f28ac941a1629cdafa10461c368d0e2782430dc
FOCKOP <79572913+FOCKOP@users.noreply.github.com>
2026-03-31 12:21:08 +0200

    Update main.py

--- main.py
+++ main.py
@@ -470,7 +470,8 @@ class BoutonConfirmerCommande(discord.ui.View):
         await interaction.response.send_message(
             f"💳 **Instructions de paiement :**\n\n"
             f"Envoie **{self.prix}€** via PayPal / Lydia / Virement à :\n"
-            f"> `TON_LIEN_PAIEMENT_ICI`\n\n"
+            f"> `FR94 3000 2028 4100 0009 1155 S71
+`\n\n"
             f"Une fois le paiement effectué, clique sur le bouton ci-dessous pour le confirmer.",
             view=BoutonPaiementEffectue(self.jeu, self.rang_dep, self.rang_cib, self.pseudo, self.prix, self.temps),
             ephemeral=True
